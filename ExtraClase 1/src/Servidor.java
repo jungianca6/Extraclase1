64a0e46ab34c35138db146c9b148a6751664bc82
dylan <dylan@MSI>
2023-08-20 22:46:47 -0600

    Master

---
+++ ExtraClase 1/src/Servidor.java
@@ -0,0 +1,85 @@
+
+import javax.swing.*;
+import java.net.*;
+import java.awt.*;
+import java.io.*;
+
+public class Servidor  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoServidor mimarco=new MarcoServidor();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+	}	
+}
+
+class MarcoServidor extends JFrame implements Runnable{
+	
+	public MarcoServidor(){
+		
+		setBounds(1200,300,280,350);				
+			
+		JPanel milamina= new JPanel();
+		
+		milamina.setLayout(new BorderLayout());
+		
+		areatexto=new JTextArea();
+		
+		milamina.add(areatexto,BorderLayout.CENTER);
+		
+		add(milamina);
+		
+		setVisible(true);
+		
+		Thread hilo=new Thread(this);
+		
+		hilo.start();
+		
+		}
+	
+	private	JTextArea areatexto;
+
+	@Override
+	/*Este metodo mantiene abierto el puerto y lo escucha
+	  en segundo plano
+	 */
+	public void run() {
+		// TODO Auto-generated method stub
+		//System.out.println("ESCUCHANDO");
+		try {
+			//Abre el puerto
+			ServerSocket servidor=new ServerSocket(9999);
+			
+			String nick,ip,texto;
+			
+			Envio mensaje_recibido;
+			
+			while(true) {
+			
+			//Acepta las conexiones
+			Socket misocket=servidor.accept();
+			//Recibe el flujo de datos
+			ObjectInputStream flujo_entrada=new ObjectInputStream(misocket.getInputStream());
+			//Mete los datos recibos dentro del flujo
+			mensaje_recibido=(Envio) flujo_entrada.readObject();
+			//Se obtienen los datos contenidos en el flujo
+			nick=mensaje_recibido.getNick();
+			ip=mensaje_recibido.getIp();
+			texto=mensaje_recibido.getTexto();
+			
+			areatexto.append("\n"+nick+": "+texto+" para "+ip);
+			
+			//Cierra la conexion
+			misocket.close();
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+
